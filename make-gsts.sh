da45d101cabf6e407fd3561080988d1930be63f6
Tina <tma03@cs.tufts.edu>
2023-02-22 23:01:08 -0500

    what

---
+++ make-gsts.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+# WOAH WOAH WOAH
+# UNLESS WE, BLUESHELL, AS A GROUP, HAVE DECIDED ON SWITCHING OUR SYNTAX TO SOMETHING DIFFERENT,
+# *****DO NOT RUN THIS SCRIPT***** unless you want to create new gold standard ASTs
+
+# It creates new gold standards, both errors and ASTs for our compiler. 
+
+# if [[ -e $# 0 ]]; then 
+#     echo "no args"
+# elif [[ -e $# 1 ]]; then 
+#     echo $1
+# fi 
+
+tests=$(Make print_succtests)
+fail_tests=$(Make print_failtests)
+
+echo "Making top level:"
+make clean
+make toplevel.native 
+
+for test in $tests 
+do 
+    echo "making goldstandard for test $test........"
+    file_name="tests/test-${test}.bs"
+    gold_standard="tests/test-${test}.gst"
+    # echo $gold_standard
+    ./toplevel.native < $file_name > $gold_standard
+done 
+
+
+# cringe fail test compilation
+for ftest in $fail_tests
+do 
+    echo "Running fail test $ftest........"
+    file_name="tests/fail-${ftest}.bs"
+    gold_standard="tests/fail-${ftest}.gst"
+    ./toplevel.native < $file_name 2> $gold_standard
+done 
+
+# bye
